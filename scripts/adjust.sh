59b9b2b6e75ec768408485fe91747128341f3474
greymd <greengregson@gmail.com>
2017-03-30 16:50:32 +0900

    Initial commit

---
+++ scripts/adjust.sh
@@ -0,0 +1,14 @@
+adjust() {
+    local _CMD='stty rows #{pane_height} cols #{pane_width} && clear'
+    local _IS_SYNC="$(tmux list-windows -F '#{window_id} #{window_active} #{pane_synchronized}' | awk '$2 == 1 && $3 == 1' | grep -c .)"
+
+    if [[ "$_IS_SYNC" == "1" ]];then
+        tmux set-window-option synchronize-panes off
+    fi
+
+    tmux list-panes -F "tmux send-keys -t #{pane_id} '${_CMD}' C-m" | sh
+
+    if [[ "$_IS_SYNC" == "1" ]];then
+        tmux set-window-option synchronize-panes on
+    fi
+}
